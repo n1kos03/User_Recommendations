a04bab2d4c73b4660474f1ab26f67843b916eecf
Nikita <nikitos.frolov.03@mail.ru>
2025-08-01 12:53:13 +0300

    Start dividing project on go modules

---
+++ services/user/go.mod
@@ -0,0 +1,3 @@
+module github.com/n1kos03/User_Recommendations/services/user
+
+go 1.24.1
---
+++ services/user/handlers/get.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (h *UserService) GETUser(c *gin.Context) {
+	users, err := h.DB.GetAllUsers()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Error getting users",
+		})
+		slog.Error("Error getting users", "Error: ", err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"users": users,
+	})
+}
